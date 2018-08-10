ffc9a1eaad47d28e85bd323966e754833f9e8c80
Penny Cornell <penny.cornell1@hotmail.com>
2018-08-10 19:33:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  const arr = []
+  for(let i = 0, l = musicians.length; i < l ; i++){
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr
+}
+
+function johnLennonFacts(facts){
+  const shoutedFacts = []
+  let i = 0
+  while(i < facts.length){
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+  return shoutedFacts
+}
+
+function iLoveTheBeatles(n){
+  const arr = []
+  
+  let i = 0
+  do {
+    console.log('I love the Beatles!')
+   } while(i < 8) 
+   return arr 
+}
